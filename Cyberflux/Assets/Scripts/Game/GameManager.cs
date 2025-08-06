1059808d19d46b31911a95c97ac41dd4498466e9
Mitch McCoy <mccoy.for.tennis@gmail.com>
2025-08-06 16:45:58 -0500

    Game Manager Setup

--- Cyberflux/Assets/Scripts/Game/GameManager.cs
+++ Cyberflux/Assets/Scripts/Game/GameManager.cs
@@ -1,7 +1,33 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    
+    public static GameManager instance;
+    
+    public GameObject player;
+
+    public bool isPaused;
+
+    // UI Stuff
+    public Image playerHPBar;
+    public TMP_Text playerHPText;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        
+
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,6 +36,36 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void GameStatePause()
+    {
+        
+    }
+
+    public void GameStateResume()
+    {
+        
+    }
+
+    public void YouLose()
+    {
+        
+    }
+
+    public void YouWin()
+    {
+        
+    }
+
+    public void UpdateUI()
+    {
+        
+    }
+
+    public void LoadLevel(int lvl)
     {
         
     }
