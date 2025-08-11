a8be4171e3276bdc3672610957e518ff76266fa2
Payton Mahoney <payton.mahoney4@gmail.com>
2025-08-11 16:01:51 -0500

    Player Controller Changes
    Added Slope Handling, Sprinting, and crouching. Also fixed a but where momentum would stack if constantly jumping.

--- Cyberflux/Assets/Scripts/Player/PlayerController.cs
+++ Cyberflux/Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IDamage
 {
     [Header("Movement")]
-    [SerializeField] float moveSpeed;
+    private float moveSpeed;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
+
 
     [SerializeField] float groundDrag;
 
+    [Header("Jumping")]
     [SerializeField] float jumpForce;
     [SerializeField] float jumpCooldown;
     [SerializeField] float airMultiplier;
     bool readyToJump;
 
+    [Header("Crouching")]
+    [SerializeField] float crouchSpeed;
+    [SerializeField] float crouchHeight;
+    private float startingHeight;
+
+
     [Header("KeyBinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Stats")]
     [SerializeField] int HP;
@@ -23,6 +35,11 @@ public class PlayerController : MonoBehaviour, IDamage
     [SerializeField] float playerHeight;
     [SerializeField] LayerMask whatIsGround;
     bool grounded;
+
+    [Header("Slope Handling")]
+    [SerializeField] float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool slopeExit;
 
 
     [SerializeField] Transform orientation;
@@ -35,7 +52,15 @@ public class PlayerController : MonoBehaviour, IDamage
 
     Rigidbody rb;
 
+    public MovementState state;
 
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
 
     void Start()
     {
@@ -43,6 +68,8 @@ public class PlayerController : MonoBehaviour, IDamage
         readyToJump = true;
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        startingHeight = transform.localScale.y;
+
     }
 
     // Update is called once per frame
@@ -52,6 +79,7 @@ public class PlayerController : MonoBehaviour, IDamage
 
         MyInput();
         SpeedControl();
+        StateHandler();
 
         //Handle Drag
         if (grounded)
@@ -81,38 +109,119 @@ public class PlayerController : MonoBehaviour, IDamage
             //Enables continuous jumping by holding donw the key
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+        
+        //Start Crouch
+        if(Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        if(Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startingHeight, transform.localScale.z);
+        }
+
     }
+
+    private void StateHandler()
+    {
+
+        //Crouching
+        if(Input.GetKeyDown(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+
+        //Sprinting
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        // Walking
+        else if(grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        //In the air
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
 
     private void MovePlayer()
     {
         //Calculate Movement Direction
         moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+       
+        //Slope Handling
+
+        if (OnSlope() && !slopeExit)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if(rb.linearVelocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+
+        }              
         //Just like controller.Move()
         if (grounded)
         {
-            rb.AddForce(moveDir * moveSpeed * 10f, ForceMode.Force);
+            Debug.Log("Grounded");
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
 
         }
+
         else if (!grounded)
         {
-            rb.AddForce(moveDir * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+            Debug.Log("In the Air");
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         }
+
+        //Turn off Gravity when on a slope (No more slidey slide)
+        rb.useGravity = !OnSlope();
     }
 
+    //To make sure you can't go faster than the selected move speed
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        //Limit Velocity if you need to 
-        if(flatVel.magnitude > moveSpeed)
+        if(OnSlope() && !slopeExit)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            //Making sure you can't move faster on a slope
+            if(rb.linearVelocity.magnitude > moveSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * moveSpeed;
+            }
+
+            
+           
+        }
+        //Making sure that you can't move faster on the ground
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            //Limit Velocity if you need to 
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
+        slopeExit = true;
         //Reset Y vel
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         
@@ -120,9 +229,28 @@ public class PlayerController : MonoBehaviour, IDamage
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
+
     private void ResetJump()
     {
         readyToJump = true;
+
+        slopeExit = false;
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+    
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDir, slopeHit.normal).normalized;
     }
 
     //Damage Interface
